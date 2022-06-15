826730a17a66a3970992f3cfc507c5ef0e119ef1
MohamedElshair <mohamed.elshair@msn.com>
2022-06-15 18:04:12 +0200

    Update Create_VM_hyper-V1.ps1

--- Create_VM_hyper-V1.ps1
+++ Create_VM_hyper-V1.ps1
@@ -19,9 +19,11 @@ $Project_Path = ("$Drive_Letter_collon" + "$Project_Name")
 
 $VM_Name = Read-Host 'Please enter your vm name'
 
-$VM_Path = ("$Project_Path" + "\" + "$VM_Name")
+$VM_Full_Name = "$Project_Name" + "-" + "$VM_Name"
 
-$VHD_Path = ("$VM_Path" + "\" + "$VM_Name.vhdx")
+$VM_Path = ("$Project_Path" + "\" + "$VM_Full_Name")
+
+$VHD_Path = ("$VM_Path" + "\" + "$VM_Full_Name.vhdx")
 
 Write-Host "Your project name is -- > $Project_Name"
 Write-Host "Your drive letter is -- > $Drive_Letter_collon"
@@ -32,7 +34,7 @@ Write-Host "Your drive letter is -- > $Drive_Letter_collon"
 
 $VM_Switches = Get-VMSwitch | Out-String
 if ($VM_Switches.Contains($Project_Name)){Write-Host "We found that your VM switch is already created before"}
-else {New-VMSwitch -Name "$ProjectName" -SwitchType Private}
+else {New-VMSwitch "$Project_Name" -SwitchType Private}
 
 ################## Create Project folder with your project name ##################
 
@@ -47,13 +49,23 @@ explorer.exe $Project_Path}
 
 #################################################################################################################################
 
-##################     Create VM connected to switch name that equal your project name ##########################################
+##################     Create VM G2 connected to switch name that equal your project name ##########################################
 
 $Test_VM_Path   = Test-Path -Path $VM_Path
-if ($Test_VM_Path.Equals($false)) {New-VM -Name "$VM_Name" -Generation 2 -MemoryStartupBytes 2GB  -NoVHD -Path "$Project_Path" -SwitchName "$Project_Name"}
+if ($Test_VM_Path.Equals($false)) {New-VM -Name "$VM_Full_Name" -Generation 2 -MemoryStartupBytes 2GB  -NoVHD -Path "$Project_Path" -SwitchName "$Project_Name"}
 else {Write-Host "We found that your VM is already created before"}
 
 #################################################################################################################################
+
+
+##################     Create VM G1 connected to switch name that equal your project name ##########################################
+
+$Test_VM_Path   = Test-Path -Path $VM_Path
+if ($Test_VM_Path.Equals($false)) {New-VM -Name "$VM_Full_Name" -Generation 1 -MemoryStartupBytes 2GB  -NoVHD -Path "$Project_Path" -SwitchName "$Project_Name"}
+else {Write-Host "We found that your VM is already created before"}
+
+#################################################################################################################################
+
 
 ##################     Create VM connected to switch name that equal your VM name ##############################################
 
@@ -70,51 +82,81 @@ if ($Test_VM_Path.Equals($false)) {New-VM -Name "$VM_Name" -Generation 2 -Memory
 else {Write-Host "We found that your VM is already created before"}
 
 ################################################################################################################################
-
-
-### Attach ISO ###
-
-$ISO_folder = Read-Host "Please enter the iso file folder path"
-$ISO = Get-ChildItem $ISO_folder\*.iso | select Name | ft -HideTableHeaders | Out-String
-Remove-VMDvdDrive -VMName $VM_Name -ControllerNumber 0 -ControllerLocation 1
-Add-VMDvdDrive -VMName $VM_Name -ControllerNumber 0 -ControllerLocation 1 -Path $ISO
-
-### Customize VM ###
-
-Set-VM -Name $VM_Name -AutomaticCheckpointsEnabled 0 -CheckpointType Standard -MemoryMaximumBytes (2GB) -MemoryMinimumBytes (1GB) -MemoryStartupBytes (2GB)
-
-### Create new VHD ###
+### Create new VHD for G1 ###
+Remove-VMScsiController -VMName $VM_Full_Name -ControllerNumber 0
 $Test_VHD_Path = Test-Path $VHD_Path
 if ( $Test_VHD_Path.Equals($false)){
 New-VHD -Path $VHD_Path -SizeBytes 100Gb -Dynamic
-Add-VMHardDiskDrive -VMName $VM_Name -ControllerLocation 0 -ControllerNumber 0 -ControllerType SCSI -Path $VHD_Path}
+Add-VMHardDiskDrive -VMName $VM_Full_Name -ControllerLocation 0 -ControllerNumber 0 -ControllerType IDE -Path $VHD_Path
+Remove-VMNetworkAdapter -VMName $VM_Full_Name
+Add-VMNetworkAdapter -IsLegacy 1  -VMName $VM_Full_Name -SwitchName $Project_Name}
+else {Write-Host "No thing to do"}
+################################################################################################################################
+
+
+### Create differencing VHD for G1 #############################################################################################
+Remove-VMScsiController -VMName $VM_Full_Name -ControllerNumber 0
+$ParentPath = Read-Host 'Please enter your main VHD path'
+$Test_VHD_Path = Test-Path $VHD_Path
+$Test_ParentVHD_Path = Test-Path $ParentPath
+if ( $Test_ParentVHD_Path.Equals($false)){
+Write-Host "No parent VHD found"}
+else {
+new-vhd -Path "$VHD_Path" -ParentPath "$ParentPath"
+Add-VMHardDiskDrive -VMName $VM_Full_Name -ControllerLocation 0 -ControllerNumber 0 -ControllerType IDE -Path $VHD_Path
+Remove-VMNetworkAdapter -VMName $VM_Full_Name
+Add-VMNetworkAdapter -IsLegacy 1  -VMName $VM_Full_Name -SwitchName $Project_Name}
+ 
+################################################################################################################################
+
+
+
+### Create new VHD for G2 #####################################################################################################
+$Test_VHD_Path = Test-Path $VHD_Path
+if ( $Test_VHD_Path.Equals($false)){
+New-VHD -Path $VHD_Path -SizeBytes 100Gb -Dynamic
+Add-VMHardDiskDrive -VMName $VM_Full_Name -ControllerLocation 0 -ControllerNumber 0 -ControllerType SCSI -Path $VHD_Path}
 else {Write-Host "No thing to do"}
 
-### Create differencing VHD ###
+### Create differencing VHD for g2  ###########################################################################################
 $ParentPath = Read-Host 'Please enter your main VHD path'
 $Test_VHD_Path = Test-Path $VHD_Path
 $Test_ParentVHD_Path = Test-Path $ParentPath
 if ( $Test_ParentVHD_Path.Equals($false)){
 Write-Host "No parent VHD found"}
 else {new-vhd -Path "$VHD_Path" -ParentPath "$ParentPath"
-Add-VMHardDiskDrive -VMName $VM_Name -ControllerLocation 0 -ControllerNumber 0 -ControllerType SCSI -Path $VHD_Path}
+Add-VMHardDiskDrive -VMName $VM_Full_Name -ControllerLocation 0 -ControllerNumber 0 -ControllerType SCSI -Path $VHD_Path}
  
  
+
+### Attach ISO ###
+
+$ISO_folder = Read-Host "Please enter the iso file folder path"
+$ISO = Get-ChildItem $ISO_folder\*.iso
+Remove-VMDvdDrive -VMName $VM_Full_Name -ControllerNumber 0 -ControllerLocation 1
+Remove-VMDvdDrive -VMName $VM_Full_Name -ControllerNumber 1 -ControllerLocation 0
+Add-VMDvdDrive -VMName $VM_Full_Name -ControllerNumber 0 -ControllerLocation 1 -Path $ISO
+Set-VMDvdDrive -VMName $VM_Full_Name -ControllerNumber 0 -ControllerLocation 1 -Path $ISO
+
+### Customize VM ###
+
+Set-VM -Name $VM_Full_Name -AutomaticCheckpointsEnabled 0 -CheckpointType Standard -MemoryMaximumBytes (2GB) -MemoryMinimumBytes (1GB) -MemoryStartupBytes (2GB)
+
 ### Boot Order DVD first ###
-$vmDVD=Get-VMDvdDrive -VMName $VM_Name 
-$vmDrive= Get-VMHardDiskDrive -VMName $VM_Name  
-$vmNIC= Get-VMNetworkAdapter -VMName $VM_Name
-Set-VMFirmware -VMName $VM_Name -EnableSecureBoot On  -BootOrder $vmDVD,$vmDrive,$vmNIC  
+$vmDVD=Get-VMDvdDrive -VMName $VM_Full_Name 
+$vmDrive= Get-VMHardDiskDrive -VMName $VM_Full_Name 
+$vmNIC= Get-VMNetworkAdapter -VMName $VM_Full_Name
+Set-VMFirmware -VMName $VM_Full_Name -EnableSecureBoot On -BootOrder $vmDVD,$vmDrive,$vmNIC  
 
 ### Boot Order VHD first ###
-$vmDVD=Get-VMDvdDrive -VMName $VM_Name 
-$vmDrive= Get-VMHardDiskDrive -VMName $VM_Name  
-$vmNIC= Get-VMNetworkAdapter -VMName $VM_Name
-Set-VMFirmware -VMName $VM_Name -EnableSecureBoot On  -BootOrder $vmDrive,$vmDVD,$vmNIC 
+$vmDVD=Get-VMDvdDrive -VMName $VM_Full_Name
+$vmDrive= Get-VMHardDiskDrive -VMName $VM_Full_Name  
+$vmNIC= Get-VMNetworkAdapter -VMName $VM_Full_Name
+Set-VMFirmware -VMName $VM_Full_Name -EnableSecureBoot On -BootOrder $vmDrive,$vmDVD,$vmNIC 
 
 
 ### Integration Services ###
-Enable-VMIntegrationService -Name "Guest Service Interface" -VMName $VM_Name 
+Enable-VMIntegrationService -Name "Guest Service Interface" -VMName $VM_Full_Name
 ### Network Apaptor for the project name ###
 Add-VMNetworkAdapter -VMName $VM_Name -SwitchName $Project_Name
 ###
