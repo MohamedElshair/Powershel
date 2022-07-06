e6ae38f7b2deb67ff85e03334458ec4701b89dd5
MohamedElshair <75900325+MohamedElshair@users.noreply.github.com>
2022-07-06 15:43:16 +0200

    Update CustomizeSVR.ps1

--- Create-VM/CustomizeSVR.ps1
+++ Create-VM/CustomizeSVR.ps1
@@ -1,9 +1,10 @@
 ﻿$Project_Name = "F2022"
 Get-VM $Project_Name*
-Start-VM -VMName "F2022-DC1"
+Start-VM -VMName "F2022-DC2"
 
 
-Enter-PSSession -VMName "F2022-DC1" -Credential (Get-Credential administrator)
+Enter-PSSession -VMName "F2022-DC2" -Credential (Get-Credential administrator)
+Enter-PSSession -VMName "F2022-DC2" -Credential (Get-Credential itoutbreak\administrator)
 
 
 Get-TimeZone *egy*
@@ -14,10 +15,10 @@ Get-NetIPInterface  -AddressFamily IPv4
 $InterfaceIndex = "6"
 Get-NetIPAddress    -AddressFamily IPv4 -InterfaceIndex $InterfaceIndex
 Remove-NetIPAddress -AddressFamily IPv4 -InterfaceIndex $InterfaceIndex
-New-NetIPAddress    -AddressFamily IPv4 -InterfaceIndex $InterfaceIndex -IPAddress 10.0.0.10 -PrefixLength 8
+New-NetIPAddress    -AddressFamily IPv4 -InterfaceIndex $InterfaceIndex -IPAddress 10.0.0.20 -PrefixLength 8
 Set-NetIPAddress    -AddressFamily IPv4 -InterfaceIndex $InterfaceIndex -IPAddress 10.0.0.10 -PrefixLength 8
 
-Set-DnsClientServerAddress -ServerAddresses 127.0.0.1 -InterfaceIndex $InterfaceIndex
+Set-DnsClientServerAddress -ServerAddresses 10.0.0.10 -InterfaceIndex $InterfaceIndex
 
 Get-WindowsFeature *back* | select name
 
@@ -25,7 +26,7 @@ Add-WindowsFeature "AD-Domain-Services" -IncludeAllSubFeature -IncludeManagement
 Add-WindowsFeature "Windows-Server-Backup" -IncludeAllSubFeature -IncludeManagementTools
 
 
-Rename-Computer -NewName DC1 -Restart -Force
+Rename-Computer -NewName DC2 -Restart -Force
 
 $DatabasePath = "c:\windows\NTDS"
 $LogPath = "c:\windows\NTDS"
@@ -33,6 +34,8 @@ $SysvolPath = "c:\windows\SYSVOL"
 
 Install-ADDSForest -DomainName Itoutbreak.net -DomainNetbiosName Itoutbreak -ForestMode WinThreshold -DomainMode WinThreshold `
 -InstallDns -DatabasePath $DatabasePath -LogPath $LogPath -SysvolPath $SysvolPath
+
+Install-ADDSDomainController -DomainName Itoutbreak.local -DatabasePath $DatabasePath -LogPath $LogPath -SysvolPath $SysvolPath -InstallDns
 
 
 
