9d3a4b894a653840293a18243d725948b5890e7c
Mohamed Elshair <mohamed.elshair@msn.com>
2022-07-13 11:34:49 +0200

    Create Create_VM_Global_Variables.ps1

---
+++ Create-VM/Create_VM_Global_Variables.ps1
@@ -0,0 +1,15 @@
+﻿### Global Variables
+
+$Global:Project_Name = "Forest2022"
+
+$Global:Drive_Letter_collon = "D" + ':\'
+
+$Global:Project_Path = ("$Drive_Letter_collon" + "$Project_Name")
+
+$Global:VM_Name="DC1"
+
+$Global:VM_Full_Name="$Project_Name" + "-" + "$VM_Name"
+
+$Global:VM_Path = ("$Project_Path" + "\" + "$VM_Full_Name")
+
+$Global:VHD_Path = ("$VM_Path" + "\" + "$VM_Full_Name.vhdx")
