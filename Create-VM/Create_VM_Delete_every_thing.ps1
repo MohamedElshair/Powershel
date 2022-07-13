9532ff9d8195b247520e84aaf4555ebfc0d0c13f
MohamedElshair <75900325+MohamedElshair@users.noreply.github.com>
2022-07-13 18:47:13 +0000

    Update Create_VM_Delete_every_thing.ps1

--- Create-VM/Create_VM_Delete_every_thing.ps1
+++ Create-VM/Create_VM_Delete_every_thing.ps1
@@ -1,7 +1,9 @@
-﻿Get-VM $Global:Project_Name*      | Stop-VM -Force
+﻿Clear-Host
 
-Get-VM $Global:Project_Name*      | Remove-VM -Force
+Get-VM $Project_Name*      | Stop-VM -Force
 
-Get-VMSwitch $Global:Project_Name | Remove-VMSwitch -Force
+Get-VM $Project_Name*      | Remove-VM -Force
 
-Remove-Item -Path $Global:Project_Path -Recurse -Force
+Get-VMSwitch $Project_Name | Remove-VMSwitch -Force
+
+Remove-Item -Path $Project_Path -Recurse -Force
