15e9d6b3c52fd28f85fad187c8ee2e278d051e0e
Mohamed Elshair <mohamed.elshair@msn.com>
2022-07-13 15:57:52 +0200

    Create Create_VM_Delete_every_thing.ps1

---
+++ Create-VM/Create_VM_Delete_every_thing.ps1
@@ -0,0 +1,7 @@
+﻿Get-VM $Global:Project_Name*      | Stop-VM -Force
+
+Get-VM $Global:Project_Name*      | Remove-VM -Force
+
+Get-VMSwitch $Global:Project_Name | Remove-VMSwitch -Force
+
+Remove-Item -Path $Global:Project_Path -Recurse -Force
