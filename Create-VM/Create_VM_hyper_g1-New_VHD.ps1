8d64de8694e091cb0c986c9bf014867bfe597499
MohamedElshair <75900325+MohamedElshair@users.noreply.github.com>
2022-07-13 19:16:11 +0000

    Update Create_VM_hyper_g1-New_VHD.ps1

--- Create-VM/Create_VM_hyper_g1-New_VHD.ps1
+++ Create-VM/Create_VM_hyper_g1-New_VHD.ps1
@@ -1,37 +1,37 @@
 ### Create VM G1 connected to switch name equal to same project name ###
 Clear-Host
-$Test_Project_Path   = Test-Path -Path $Global:Project_Path
+$Test_Project_Path   = Test-Path -Path $Project_Path
 if ($Test_Project_Path.Equals($false))
 {
-New-Item -ItemType Directory -Name $Global:Project_Name -Path $Global:Drive_Letter_collon
-Write-Host "We made a Project Folder on behalf of you to create your VM's inside it, You can find it under this path $Global:Project_Path"
+New-Item -ItemType Directory -Name $Project_Name -Path $Drive_Letter_collon
+Write-Host "We made a Project Folder on behalf of you to create your VM's inside it, You can find it under this path $Project_Path"
 }
 elseif ($Test_Project_Path.Equals($true))
-{Write-Host "We found that Project Folder is already created before, You can find it under this path $Global:Project_Path"}
+{Write-Host "We found that Project Folder is already created before, You can find it under this path $Project_Path"}
 
 
 Clear-Host
-$Test_VM_Path   = Test-Path -Path $Global:VM_Path
+$Test_VM_Path   = Test-Path -Path $VM_Path
 if ($Test_VM_Path.Equals($false))
 {
-New-VM -Name "$Global:VM_Full_Name" -Generation 1 -MemoryStartupBytes 2GB  -NoVHD -Path "$Global:VM_Path" -SwitchName "$Global:Project_Name"
-Set-VM -Name $Global:VM_Full_Name -AutomaticCheckpointsEnabled 0 -CheckpointType Standard
-Set-VM -Name $Global:VM_Full_Name -DynamicMemory -MemoryMinimumBytes 1GB -MemoryMaximumBytes 2GB
-Enable-VMIntegrationService -Name "Guest Service Interface" -VMName $Global:VM_Full_Name
-Disable-VMIntegrationService -Name "Time Synchronization" -VMName $Global:VM_Full_Name
+New-VM -Name "$VM_Full_Name" -Generation 1 -MemoryStartupBytes 2GB  -NoVHD -Path "$VM_Path" -SwitchName "$Project_Name"
+Set-VM -Name $VM_Full_Name -AutomaticCheckpointsEnabled 0 -CheckpointType Standard
+Set-VM -Name $VM_Full_Name -DynamicMemory -MemoryMinimumBytes 1GB -MemoryMaximumBytes 2GB
+Enable-VMIntegrationService -Name "Guest Service Interface" -VMName $VM_Full_Name
+Disable-VMIntegrationService -Name "Time Synchronization" -VMName $VM_Full_Name
 }
 else {}
 
 
 #### Create new VHD for G1 ###
 Clear-Host
-Remove-VMScsiController -VMName $Global:VM_Full_Name -ControllerNumber 0
-$Test_VHD_Path = Test-Path $Global:VHD_Path
+Remove-VMScsiController -VMName $VM_Full_Name -ControllerNumber 0
+$Test_VHD_Path = Test-Path $VHD_Path
 if ( $Test_VHD_Path.Equals($false)){
-New-VHD -Path $Global:VHD_Path -SizeBytes 100Gb -Dynamic
-Add-VMHardDiskDrive -VMName $Global:VM_Full_Name -ControllerLocation 0 -ControllerNumber 0 -ControllerType IDE -Path $Global:VHD_Path
-Remove-VMNetworkAdapter -VMName $Global:VM_Full_Name
-Add-VMNetworkAdapter -IsLegacy 1  -VMName $Global:VM_Full_Name -SwitchName $Global:Project_Name}
+New-VHD -Path $VHD_Path -SizeBytes 100Gb -Dynamic
+Add-VMHardDiskDrive -VMName $VM_Full_Name -ControllerLocation 0 -ControllerNumber 0 -ControllerType IDE -Path $VHD_Path
+Remove-VMNetworkAdapter -VMName $VM_Full_Name
+Add-VMNetworkAdapter -IsLegacy 1  -VMName $VM_Full_Name -SwitchName $Project_Name}
 else {Write-Host "No thing to do"}
 
 
@@ -39,15 +39,15 @@ else {Write-Host "No thing to do"}
 Clear-Host
 $ISO_folder = Read-Host "Please enter the iso file folder path"
 $ISO = Get-ChildItem $ISO_folder\*.iso
-Remove-VMDvdDrive -VMName $Global:VM_Full_Name -ControllerNumber 0 -ControllerLocation 1
-Remove-VMDvdDrive -VMName $Global:VM_Full_Name -ControllerNumber 1 -ControllerLocation 0
-Add-VMDvdDrive -VMName $Global:VM_Full_Name -ControllerNumber 0 -ControllerLocation 1 -Path $ISO
-Set-VMDvdDrive -VMName $Global:VM_Full_Name -ControllerNumber 0 -ControllerLocation 1 -Path $ISO
+Remove-VMDvdDrive -VMName $VM_Full_Name -ControllerNumber 0 -ControllerLocation 1
+Remove-VMDvdDrive -VMName $VM_Full_Name -ControllerNumber 1 -ControllerLocation 0
+Add-VMDvdDrive -VMName $VM_Full_Name -ControllerNumber 0 -ControllerLocation 1 -Path $ISO
+Set-VMDvdDrive -VMName $VM_Full_Name -ControllerNumber 0 -ControllerLocation 1 -Path $ISO
 
 
 
 ## start VM's ###
-Get-VM -Name *$Global:Project_Name* | Start-VM
+Get-VM -Name *$Project_Name* | Start-VM
 
 
 
