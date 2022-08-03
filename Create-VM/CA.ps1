1f69de58e73c62391f65a5b9ac2e168a884d053e
Mohamed Elshair <mohamed.elshair@msn.com>
2022-08-03 19:01:38 +0200

    Update CA.ps1

--- Create-VM/CA.ps1
+++ Create-VM/CA.ps1
@@ -31,3 +31,13 @@ Certutil -setreg CA\CRLOverlapPeriod "Hours"
 ### To define Validity Period Units for all issued certificates by this CA, type following command and then press Enter. In this lab, the Enterprise Issuing CA should receive a 10 year lifetime for its CA certificate. To configure this, run the following commands from an administrative command prompt:
 Certutil -setreg CA\ValidityPeriodUnits 10
 Certutil -setreg CA\ValidityPeriod "Years"
+
+### Configure the AIA
+certutil -setreg CA\CACertPublicationURLs "1:C:\Windows\system32\CertSrv\CertEnroll\%1_%3%4.crt\n2:ldap:///CN=%7,CN=AIA,CN=Public Key Services,CN=Services,%6%11\n2:http://pki.Itoutbreak.net/CertEnroll/%1_%3%4.crt"
+
+certutil -getreg CA\CACertPublicationURLs
+
+### Configure the CDP
+certutil -setreg CA\CRLPublicationURLs "1:C:\Windows\system32\CertSrv\CertEnroll\%3%8%9.crl\n10:ldap:///CN=%7%8,CN=%2,CN=CDP,CN=Public Key Services,CN=Services,%6%10\n2:http://pki.Itoutbreak.net/CertEnroll/%3%8%9.crl"
+
+certutil -getreg CA\CRLPublicationURLs
