0f2850a9909ca45936bbb345ebdcf876e61239be
Mohamed Elshair <mohamed.elshair@msn.com>
2022-08-03 18:15:36 +0200

    Create CA.ps1

---
+++ Create-VM/CA.ps1
@@ -0,0 +1,17 @@
+﻿
+### Create a CAPolicy.inf for the standalone offline root CA
+
+[Version]
+
+Signature="$Windows NT$"
+
+[Certsrv_Server]
+
+RenewalKeyLength=2048 ; recommended 4096
+
+RenewalValidityPeriod=Years
+
+RenewalValidityPeriodUnits=20
+
+AlternateSignatureAlgorithm=0
+
