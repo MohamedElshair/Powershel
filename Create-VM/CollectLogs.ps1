146a56940f38a57f3d21a23e3ed6c838ac0a34ca
MohamedElshair <75900325+MohamedElshair@users.noreply.github.com>
2022-08-15 18:51:07 +0200

    Update CollectLogs.ps1

--- Create-VM/CollectLogs.ps1
+++ Create-VM/CollectLogs.ps1
@@ -1,4 +1,17 @@
-﻿$Testpath = Test-Path  'C:\CollectLogs.ps1'
+﻿$Ports = ("53","135","88","389")
+#53  --> DNS
+#135 --> RPC Locator
+#88  --> Kerberos
+#389 --> LDAP
+cls
+foreach ($Ports in $Ports)
+{
+Test-NetConnection "DC name" -Port $Ports
+}
+
+
+
+$Testpath = Test-Path  'C:\CollectLogs.ps1'
 
 if ($Testpath -eq $true )
 {
@@ -22,20 +35,11 @@ $Date = Get-Date -Format 'dd.MMMM.yy   HH.mm.ss'
 $folderFullName  = "MicrosoftLogs" + "   " + "$Date"
 
 
-$Ports = ("53","135","88","389")
-#53  --> DNS
-#135 --> RPC Locator
-#88  --> Kerberos
-#389 --> LDAP
-cls
-foreach ($Ports in $Ports)
-{
-Test-NetConnection "DC name" -Port $Ports
-}
-
 New-Item "$folderFullName" -ItemType directory
 New-Item "$folderFullName\$HostName-MainInfo.txt" -ItemType file
 New-Item "$folderFullName\$HostName-DFS.txt" -ItemType file
+New-Item "$folderFullName\$HostName-ComputerInfo.txt" -ItemType file
+New-Item "$folderFullName\$HostName-Services.txt" -ItemType file
 New-Item "$folderFullName\$HostName-LOGS" -ItemType Directory
 New-Item "$folderFullName\$HostName-Events" -ItemType Directory
 New-Item "$folderFullName\$HostName-Replication" -ItemType Directory
@@ -51,6 +55,19 @@ Copy-Item 'C:\Windows\System32\Winevt\Logs\DNS Server.evtx' "c:\$folderFullName\
 Copy-Item 'C:\Windows\System32\Winevt\Logs\System.evtx' "c:\$folderFullName\$HostName-Events\$HostName-System.evtx" -Force
 Copy-Item 'C:\Windows\System32\Winevt\Logs\Security.evtx' "c:\$folderFullName\$HostName-Events\$HostName-Security.evtx" -Force
 
+Get-ComputerInfo > c:\$folderFullName\$HostName-ComputerInfo.txt
+
+$NTDS=cmd.exe /c "sc query ntds"
+$netlogon=cmd.exe /c "sc query netlogon"
+$lanman=cmd.exe /c "sc query lanmanworkstation"
+$dns=cmd.exe /c "sc query dns"
+$rpc=cmd.exe /c "sc query rpcss"
+
+Add-Content "c:\$folderFullName\$HostName-Services.txt" $NTDS," " ,$netlogon," ",$lanman," ",$dns," ",$rpc
+
+### Registery values ###
+$Get-ItemProperty -Path  Registry::HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Netlogon\Parameters\
+Get-ItemProperty -Path  Registry::HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\NTDS\Parameters\
 
 $FSMO     = netdom query fsmo
 Add-Content "$folderFullName\$HostName-MainInfo.txt" $HostName," ",$FSMO," ",$getadcomputer," ",$w32tm," ",$whoamiupn," ",$whoamiGroups
@@ -70,6 +87,7 @@ $DFSReplicationState = dfsrdiag.exe /ReplicationState
 
 Add-Content "c:\$folderFullName\$HostName-DFS.txt" "$DFSglobalstate"," ",$DFSmigrationstate," ","$DFSReplicationState"
 Move-Item "c:\$folderFullName\$HostName-DFS.txt" "$folderFullName\$HostName-Replication"
+
 }
 
 Add-Content -Value $content1 -Path C:\CollectLogs.ps1
