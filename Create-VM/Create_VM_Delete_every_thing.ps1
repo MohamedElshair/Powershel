5d8bcbfc83366173c7a1e370147fad095e085e7d
MohamedElshair <75900325+MohamedElshair@users.noreply.github.com>
2022-08-17 13:25:46 +0200

    Update Create_VM_Delete_every_thing.ps1

--- Create-VM/Create_VM_Delete_every_thing.ps1
+++ Create-VM/Create_VM_Delete_every_thing.ps1
@@ -1,9 +1,20 @@
-﻿Clear-Host
+﻿c: ; cd\ ; Clear-Host
 
 Get-VM $Project_Name*      | Stop-VM -Force
 
 Get-VM $Project_Name*      | Remove-VM -Force
 
-Get-VMSwitch $Project_Name | Remove-VMSwitch -Force
+$VM_Switches = Get-VMSwitch | Out-String
+if ($VM_Switches.Contains($Project_Name))
+{
+Get-VMSwitch $Project_Name | Remove-VMSwitch -Force}
+else 
+{}
 
-Remove-Item -Path $Project_Path -Recurse -Force
+$Test_Project_Path   = Test-Path -Path $Project_Path
+if ($Test_Project_Path.Equals($false))
+{}
+elseif ($Test_Project_Path.Equals($true))
+{Remove-Item -Path $Project_Path -Recurse -Force}
+
+Write-Host "Every thing is deleted"
