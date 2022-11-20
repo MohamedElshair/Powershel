c9f1cac5066662e5ec6db847f61fca5c2520dce7
MohamedElshair <75900325+MohamedElshair@users.noreply.github.com>
2022-11-20 10:42:36 +0200

    Create Get-AdComputers.ps1

---
+++ AD/Get-AdComputers.ps1
@@ -0,0 +1,12 @@
+﻿$FileName = " " ; $FileName = Read-Host 'Enter file name'
+$ExportPath = " " ; $ExportPath = "c:\$FileName.csv"
+$Server = " " ; $Server = 'dc16.scb.local'
+
+
+
+
+Get-ADComputer -Filter * -Properties * -Server $Server  |   Where-Object Enabled -EQ $false |select DistinguishedName,DNSHostName,Enabled,PasswordLastSet | Export-Csv $ExportPath -NoTypeInformation
+
+& $ExportPath
+
+del $ExportPath
