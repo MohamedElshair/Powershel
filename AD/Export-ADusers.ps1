d1b171dfcaeac8df9a904c0e311517e691e5d649
MohamedElshair <75900325+MohamedElshair@users.noreply.github.com>
2022-11-20 10:42:47 +0200

    Create Export-ADusers.ps1

---
+++ AD/Export-ADusers.ps1
@@ -0,0 +1,31 @@
+﻿$ExportPath =  Read-Host 'Enter you Export path'
+
+$ExportFileName = 'ITBranchUsers.csv'
+
+$ExportPathPredifined = 'C:\Users\mohamed.elshair\Desktop'
+
+$ExportPathFull = "$ExportPathPredifined\$ExportFileName"
+
+$SearchBase = 'OU=IT,DC=scb,DC=local'
+
+
+$TestExportFileName = Test-Path $ExportPathFull
+
+if  ($TestExportFileName -eq $true) 
+{ 
+rm  $ExportPathFull
+Get-ADUser -Filter * -Properties * -SearchBase $SearchBase  | select SamAccountName,UserPrincipalName,telephoneNumber  | Export-Csv $ExportPathFull -NoTypeInformation
+}
+else 
+{Get-ADUser -Filter * -Properties * -SearchBase $SearchBase  | select SamAccountName,UserPrincipalName,telephoneNumber  | Export-Csv $ExportPathFull -NoTypeInformation}
+
+& $ExportPathFull
+
+
+
+
+
+Get-ADUser -Identity kareem.essam  -Properties *  | select SamAccountName,UserPrincipalName,telephoneNumber,proxyAddresses | Export-Csv "$ExportPathPredifined\test.csv" -NoTypeInformation -Verbose
+
+
+Get-ADUser -Identity kareem.essam -Properties proxyaddresses | select name, @{L=’ProxyAddress_1′; E={$_.proxyaddresses[0]}}, @{L=’ProxyAddress_2′;E={$_.ProxyAddresses[1]}} | Export-Csv "$ExportPathPredifined\test.csv" -NoTypeInformation -Verbose
