11da179f73091e30baf3cb293228e6da02f1e325
MohamedElshair <75900325+MohamedElshair@users.noreply.github.com>
2023-01-14 10:11:47 +0200

    Update CustomizeSVR.ps1

--- Create-VM/CustomizeSVR.ps1
+++ Create-VM/CustomizeSVR.ps1
@@ -1,42 +1,44 @@
 ﻿Clear-Host ; cd\
-$Project_Name             = $Project_Name
-$Net_BIOS_Name            = 'Itoutbreak'
-$Domain_Name              = "$Net_BIOS_Name" + "." + "net"
-$ComputerName             = "DC1"
-$VM_Name                  = "$Project_Name" + "-" + "$ComputerName"
-$LocalUserNameSRV         = 'administrator'
-$LocalUserNameCLT         = '.\admin'
-$DomainUserName           = "$Net_BIOS_Name\$LocalUserNameSRV"
-$Password                 = ConvertTo-SecureString -String 'P@$$w0rd' -AsPlainText -Force
-$LocalCredentialServer    = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $LocalUserNameSRV,$Password
-$LocalCredentialClient    = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $LocalUserNameCLT,$Password
-$DomainCredential         = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $DomainUserName,$Password
+[string]$Project_Name             = $Project_Name
+[string]$Net_BIOS_Name            = 'Itoutbreak'
+$Domain_Name                      = "$Net_BIOS_Name" + "." + "net"
+[string]$ComputerName             = 'ROOTCA'
+$VM_Name                          = "$Project_Name" + "-" + "$ComputerName"
+[string]$LocalUserNameSRV         = 'administrator'
+[string]$LocalUserNameCLT         = '.\admin'
+$DomainUserName                   = "$Net_BIOS_Name\$LocalUserNameSRV"
+$Password                         = ConvertTo-SecureString -String 'P@$$w0rd' -AsPlainText -Force
+$LocalCredentialServer            = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $LocalUserNameSRV,$Password
+$LocalCredentialClient            = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $LocalUserNameCLT,$Password
+$DomainCredential                 = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $DomainUserName,$Password
 
-$DatabasePath             = 'c:\windows\NTDS'
-$LogPath                  = 'c:\windows\NTDS'
-$SysvolPath               = 'c:\windows\SYSVOL'
-$StartRange               = '10.0.0.100' 
-$EndRange                 = '10.0.0.200'
-$DNS_Server               = '10.0.0.10'
-$DC1_IPAddress = ' ' ; $DC1_IPAddress = '10.0.0.10'
+[string]$DatabasePath             = 'c:\windows\NTDS'
+[string]$LogPath                  = 'c:\windows\NTDS'
+[string]$SysvolPath               = 'c:\windows\SYSVOL'
+[string]$StartRange               = '10.0.0.100' 
+[string]$EndRange                 = '10.0.0.200'
+[string]$DNS_Server               = '10.0.0.10'
+$DC1_IPAddress  = ' ' ; $DC1_IPAddress  = '10.0.0.10'
+$SVR1_IPAddress = ' ' ; $SVR1_IPAddress = '10.0.0.50'
 hostname ; whoami
 
 
-cls ; Get-VM $Project_Name*
+Clear-Host ; Get-VM $Project_Name*
 Start-VM -VMName $VM_Name
 
 
 
 ### Logon to local machine "Server" ###
-Enter-PSSession -VMName "$VM_Name" -Credential $LocalCredentialServer 
-Enter-PSSession -VMName "WSUS" -Credential $LocalCredentialServer 
+Enter-PSSession -VMName $VM_Name -Credential $LocalCredentialServer 
+Enter-PSSession -VMName WSUS     -Credential $LocalCredentialServer 
 ### Logon to local machine "Client" ###
-Enter-PSSession -VMName "$VM_Name" -Credential $LocalCredentialClient
+Enter-PSSession -VMName $VM_Name -Credential $LocalCredentialClient
 ### Logon to domain joined machine ###
-Enter-PSSession -VMName "$VM_Name" -Credential $DomainCredential
+Enter-PSSession -VMName $VM_Name -Credential $DomainCredential
 
 
 cd\ ; Clear ; hostname ; whoami
+Exit
 
 
 
@@ -50,68 +52,80 @@ Restart-Computer -Force
 
 Get-TimeZone *egy*
 
-cls ; Set-TimeZone -Id "Egypt Standard Time" ; Get-TimeZone
+Clear-Host ; Set-TimeZone -Id "Egypt Standard Time" ; Get-TimeZone
 
-cls ; ipconfig /all
+Clear-Host ; ipconfig /all
 
 Get-NetIPAddress
 
 # Rename local Network Adaptor
 $CSV_Path = ' ' ; $CSV_Path = 'c:\NetAdapter.csv'
 ########################
-cls ; Get-NetAdapter| select Name,ifIndex | Export-Csv "$CSV_Path" -Force
-cls ; $NetAdapter = ' '            ; $NetAdapter = Import-Csv "$CSV_Path" ; cls ; $NetAdapter
-cls ; $InterfaceIndexifIndex = ' ' ; $InterfaceIndexifIndex = ($NetAdapter.ifIndex)
-cls ; $InterfaceName = ' '         ; $InterfaceName = ($NetAdapter.Name)
+Clear-Host ; Get-NetAdapter| select Name,ifIndex | Export-Csv "$CSV_Path" -Force
+Clear-Host ; $NetAdapter     = ' '        ; $NetAdapter     = Import-Csv "$CSV_Path" ; Clear-Host ; $NetAdapter
+Clear-Host ; $InterfaceIndex = ' '        ; $InterfaceIndex = ($NetAdapter.ifIndex)
+Clear-Host ; $InterfaceName  = ' '        ; $InterfaceName  = ($NetAdapter.Name)
 
 $line = " "
 foreach ( $Line in $InterfaceName ) {Get-NetIPInterface "$Line" -AddressFamily IPv4}
 
-cls ; Get-NetAdapter -Name "$InterfaceName" | Rename-NetAdapter -NewName "$Domain_Name"
+Clear-Host ; Get-NetAdapter -Name "$InterfaceName" | Rename-NetAdapter -NewName "$Domain_Name"
 
 # Rename external Network Adaptor
 ########################
-cls ; Get-NetAdapter| select Name,ifIndex | Export-Csv c:\NetAdapter.csv -Force
-cls ; $NetAdapter = ' '            ; $NetAdapter = Import-Csv C:\NetAdapter.csv ; cls ; $NetAdapter
+Clear-Host ; Get-NetAdapter| select Name,ifIndex | Export-Csv c:\NetAdapter.csv -Force
+Clear-Host ; $NetAdapter = ' '            ; $NetAdapter = Import-Csv C:\NetAdapter.csv ; Clear-Host ; $NetAdapter
 $ExternalNIC = Read-Host 'Enter your external NIC index number'
 
-cls ; Get-NetAdapter -Name 'Ethernet' | Rename-NetAdapter -NewName 'External'
-cls ; Get-NetIPInterface "$InterfaceName" -AddressFamily IPv4
+Clear-Host ; Get-NetAdapter -Name 'Ethernet' | Rename-NetAdapter -NewName 'External'
+Clear-Host ; Get-NetIPInterface "$InterfaceName" -AddressFamily IPv4
 
 ## Set DC1 internal IP Address configuration
 #####################==============
-cls ; New-NetIPAddress -AddressFamily 'IPv4' -InterfaceIndex "$InterfaceIndexifIndex" -IPAddress "$DC1_IPAddress" -PrefixLength '8'
+Clear-Host ; New-NetIPAddress -AddressFamily 'IPv4' -InterfaceIndex "$InterfaceIndex" -IPAddress "$DC1_IPAddress" -PrefixLength '8'
 
-cls ; Set-DnsClientServerAddress -ServerAddresses "$DNS_Server"     -InterfaceIndex "$InterfaceIndex"
+Clear-Host ; Set-DnsClientServerAddress -ServerAddresses "$DNS_Server"     -InterfaceIndex "$InterfaceIndex"
 
-cls ; Get-NetIPAddress -AddressFamily 'IPv4' -InterfaceIndex "$InterfaceIndexifIndex" | select IPAddress | ft -AutoSize
+Clear-Host ; Get-NetIPAddress -AddressFamily 'IPv4' -InterfaceIndex "$InterfaceIndex" | select IPAddress | ft -AutoSize
 Get-DnsClientServerAddress -InterfaceIndex "$InterfaceIndex" -AddressFamily 'IPv4' 
 
-cls ; Remove-Item 'C:\NetAdapter.csv' -Force
+Clear-Host ; Remove-Item 'C:\NetAdapter.csv' -Force
 
 ## Set DC1 external IP Address configuration
 #####################==============
-cls ; New-NetIPAddress -AddressFamily 'IPv4' -InterfaceIndex "$ExternalNIC" -IPAddress "$DC1_IPAddress" -PrefixLength '8'
+Clear-Host ; New-NetIPAddress -AddressFamily 'IPv4' -InterfaceIndex $ExternalNIC -IPAddress $DC1_IPAddress -PrefixLength '8'
 
-cls ; Set-DnsClientServerAddress -ServerAddresses "$DNS_Server"     -InterfaceIndex "$InterfaceIndex"
+Clear-Host ; Set-DnsClientServerAddress -ServerAddresses "$DNS_Server"   -InterfaceIndex "$InterfaceIndex"
 
-cls ; Get-NetIPAddress -AddressFamily 'IPv4' -InterfaceIndex "$InterfaceIndexifIndex" | select IPAddress | ft -AutoSize
+Clear-Host ; Get-NetIPAddress -AddressFamily 'IPv4' -InterfaceIndex "$InterfaceIndex" | select IPAddress | ft -AutoSize
 Get-DnsClientServerAddress -InterfaceIndex "$InterfaceIndex" -AddressFamily 'IPv4' 
 
-cls ; Remove-Item 'C:\NetAdapter.csv' -Force
+Clear-Host ; Remove-Item 'C:\NetAdapter.csv' -Force
+
+
+## Set Server1 internal IP Address configuration
+#####################==============
+Clear-Host ; New-NetIPAddress -AddressFamily 'IPv4' -InterfaceIndex $InterfaceIndex -IPAddress $SVR1_IPAddress -PrefixLength '8'
+
+Clear-Host ; Set-DnsClientServerAddress -ServerAddresses "$DNS_Server"     -InterfaceIndex "$InterfaceIndex"
+
+Clear-Host ; Get-NetIPAddress -AddressFamily 'IPv4' -InterfaceIndex $InterfaceIndex | select IPAddress | ft -AutoSize
+Get-DnsClientServerAddress -InterfaceIndex "$InterfaceIndex" -AddressFamily 'IPv4' 
+
+Clear-Host ; Remove-Item 'C:\NetAdapter.csv' -Force
 
 ####################################################################################################################################
 
 Get-NetIPInterface -InterfaceIndex 15 -AddressFamily IPv4
 
-cls ; Get-NetIPAddress -AddressFamily IPv4 -InterfaceIndex $InterfaceIndex
+Clear-Host ; Get-NetIPAddress -AddressFamily IPv4 -InterfaceIndex $InterfaceIndex
 
 Remove-NetIPAddress -AddressFamily IPv4 -InterfaceIndex $InterfaceIndex
-cls ; New-NetIPAddress    -AddressFamily IPv4 -InterfaceIndex $InterfaceIndex -IPAddress 10.0.0.10 -PrefixLength 8 ; cls ; ipconfig /all
-cls ; New-NetIPAddress    -AddressFamily IPv4 -InterfaceIndex $InterfaceIndex -IPAddress 10.0.0.10 -PrefixLength 8 ; cls ; ipconfig /all
-cls ; New-NetIPAddress    -AddressFamily IPv4 -InterfaceIndex $InterfaceIndex -IPAddress 10.0.0.20 -PrefixLength 8 ; cls ; ipconfig /all
-cls ; New-NetIPAddress    -AddressFamily IPv4 -InterfaceIndex $InterfaceIndex -IPAddress 10.0.0.40 -PrefixLength 8 ; cls ; ipconfig /all
-cls ; Set-DnsClientServerAddress -ServerAddresses $DNS_Server -InterfaceIndex $InterfaceIndex ; cls ; ipconfig /all
+Clear-Host ; New-NetIPAddress    -AddressFamily IPv4 -InterfaceIndex $InterfaceIndex -IPAddress 10.0.0.10 -PrefixLength 8 ; Clear-Host ; ipconfig /all
+Clear-Host ; New-NetIPAddress    -AddressFamily IPv4 -InterfaceIndex $InterfaceIndex -IPAddress 10.0.0.10 -PrefixLength 8 ; Clear-Host ; ipconfig /all
+Clear-Host ; New-NetIPAddress    -AddressFamily IPv4 -InterfaceIndex $InterfaceIndex -IPAddress 10.0.0.20 -PrefixLength 8 ; Clear-Host ; ipconfig /all
+Clear-Host ; New-NetIPAddress    -AddressFamily IPv4 -InterfaceIndex $InterfaceIndex -IPAddress 10.0.0.40 -PrefixLength 8 ; Clear-Host ; ipconfig /all
+Clear-Host ; Set-DnsClientServerAddress -ServerAddresses $DNS_Server -InterfaceIndex $InterfaceIndex ; Clear-Host ; ipconfig /all
 
 New-NetIPAddress    -AddressFamily IPv4 -InterfaceIndex $InterfaceIndex -IPAddress 10.0.0.30 -PrefixLength 8
 New-NetIPAddress    -AddressFamily IPv4 -InterfaceIndex $InterfaceIndex -IPAddress 10.0.0.100 -PrefixLength 8
@@ -120,25 +134,29 @@ Set-NetIPAddress    -AddressFamily IPv4 -InterfaceIndex $InterfaceIndex -IPAddre
 
 
 ### List features ###
-cls ; Get-WindowsFeature | Where-Object InstallState -NE installed | select Name
+Clear-Host ; Get-WindowsFeature | Where-Object InstallState -NE installed | select Name
 
-cls ; Get-WindowsFeature | Where-Object InstallState -EQ installed | select name
+Clear-Host ; Get-WindowsFeature | Where-Object InstallState -EQ installed | select name
 
 ### Add Roles ###
 # 'AD-Domain-Services','FS-DFS-Replication'
 # 'AD-Certificate','ADCS-Cert-Authority','ADCS-Web-Enrollment'
 # 'Windows-Server-Backup','Web-Server','RemoteAccess','DHCP'
 
-cls ; $Features = ' ' ; $Features = Get-WindowsFeature ('AD-Domain-Services','FS-DFS-Replication')
-cls ; $Features = ' ' ; $Features = Get-WindowsFeature ('Windows-Server-Backup')
-cls ; $Features = ' ' ; $Features = Get-WindowsFeature ('DHCP')
+Clear-Host ; $Features = ' ' ; $Features = Get-WindowsFeature 'AD-Domain-Services','FS-DFS-Replication'
+Clear-Host ; $Features = ' ' ; $Features = Get-WindowsFeature 'Windows-Server-Backup'
+Clear-Host ; $Features = ' ' ; $Features = Get-WindowsFeature 'DHCP'
+Clear-Host ; $Features = ' ' ; $Features = Get-WindowsFeature 'DHCP','Windows-Server-Backup'
+Clear-Host ; $Features = ' ' ; $Features = Get-WindowsFeature 'AD-Certificate','ADCS-Cert-Authority'
 
 $Features.InstallState -eq 'installed'
 
 foreach ($Feature in $Features){
 if ($Feature.InstallState -ne 'installed'){
-Install-WindowsFeature "$Feature" -IncludeAllSubFeature -IncludeManagementTools}
-else {cls ; Write-Host Feature is already installed}}
+Install-WindowsFeature -Name $Feature -IncludeAllSubFeature -IncludeManagementTools}
+else {Clear-Host ; Write-Host Feature is already installed}}
+
+
 
 
 #### DHCP ####
@@ -151,11 +169,11 @@ Add-DhcpServerInDC -DnsName $ComputerName -Confirm
 
 ### Remove Roles ###
 
-cls ; $Features = Get-WindowsFeature ('AD-Domain-Services','FS-DFS-Replication','GPMC')
+Clear-Host ; $Features = Get-WindowsFeature ('AD-Domain-Services','FS-DFS-Replication','GPMC')
 foreach ($Features in $Features){
 if ($Features.InstallState -eq 'installed'){
 Remove-WindowsFeature $Features -IncludeManagementTools}
-else {cls ; Write-Host Feature is not installed}}
+else {Clear-Host ; Write-Host Feature is not installed}}
 
 hostname
 
@@ -163,7 +181,7 @@ Restart-Computer -Force
 
 
 ### Promote First Forest ###
-cls ;Install-ADDSForest -DomainName $Domain_Name -DomainNetbiosName $Net_BIOS_Name -ForestMode WinThreshold -DomainMode WinThreshold -InstallDns -DatabasePath $DatabasePath -LogPath $LogPath -SysvolPath $SysvolPath -SafeModeAdministratorPassword $Password -Force
+Clear-Host ;Install-ADDSForest -DomainName $Domain_Name -DomainNetbiosName $Net_BIOS_Name -ForestMode WinThreshold -DomainMode WinThreshold -InstallDns -DatabasePath $DatabasePath -LogPath $LogPath -SysvolPath $SysvolPath -SafeModeAdministratorPassword $Password -Force
 
 Install-ADDSDomainController -DomainName $Net_BIOS_Name -DatabasePath $DatabasePath -LogPath $LogPath -SysvolPath $SysvolPath -SafeModeAdministratorPassword $Password -InstallDns -Force -Credential $DomainCredential
 
