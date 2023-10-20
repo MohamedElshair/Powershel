6d3c1a3f422b15241d0ea1db2c3c10e3799141de
MohamedElshair <mohamed.elshair@msn.com>
2023-10-20 19:01:53 +0300

    Update Create_VM_Global_Variables.ps1

--- Create-VM/Create_VM_Global_Variables.ps1
+++ Create-VM/Create_VM_Global_Variables.ps1
@@ -1,43 +1,70 @@
 ﻿### Global Variables ###
-#### Set Available drive letter ####
+
+#region Set available drive letter
 Clear-Host
-$Drive_Letter               = Read-Host 'Enter you project driver letter only'
-$Global:Project_Name        = Read-Host 'Enter your project name '
-$Global:VM_Name             = Read-Host 'Please enter your vm name'
-$Global:Project_Path        = ("$Global:Drive_Letter_collon" + "$Project_Name") 
+$Global:DL                  = Read-Host 'Enter you project driver letter'
+$Global:Drive_Letter        = $DL.Substring(0, 1)
 $Global:Drive_Letter_collon = "$Drive_Letter" + ':\'
-$Global:VM_Full_Name        = "$Global:Project_Name" + "-" + "$Global:VM_Name"
-$Global:VM_Path             = ("$Global:Project_Path" + "\" + "$Global:VM_Full_Name")
-$Test_Drive                 = Test-path $Global:Drive_Letter_collon
-$global:VHD_Path            = ("$Global:VM_Path" + "\" + "$Global:VM_Full_Name.vhd")
-$global:VHDx_Path           = ("$Global:VM_Path" + "\" + "$Global:VM_Full_Name.vhdx")
+#endregion
+
+#region Set project name and related folders
+Clear-Host ; $Global:Project_Name = Read-Host 'Enter your project name'
+$Global:Project_Path = ("$Drive_Letter_collon" + "$Project_Name") 
+
+#endregion
+
+#region Set export folder
+Clear-Host
+$Global:ExportFolderPath     = 'c:\Export'
+$Global:TestExportFolderPath = Test-Path $ExportFolderPath
+
+if ($TestExportFolderPath -eq $false) 
+{New-Item -Name 'Export' -ItemType Directory -Path $Drive_Letter_collon -Force}
+#endregion
+
+#region Set VM names in one text file ####
+Clear-Host
+$Global:AskForVMNumber      = Read-Host "How many numbers of your project VM's"
+
+$Global:VMTXTFileNAme       = 'VM.txt'
+$Global:VMTXTPath           = "$ExportFolderPath\$VMTXTFileNAme"
+$Global:TestExportVMTXTPath = Test-Path $VMTXTPath
+
+if ($TestExportVMTXTPath -eq $true) 
+{
+Remove-Item -Path $VMTXTPath -Force
+New-Item    -Path $ExportFolderPath -Name $VMTXTFileNAme -ItemType File
+} 
+Else 
+{
+New-Item    -Path $ExportFolderPath -Name $VMTXTFileNAme -ItemType File
+}
 
 Clear-Host
 
-if ($Test_Drive.Equals($true)){
-Clear-Host
-Write-Host 'You Drive is set to' "$Global:Drive_Letter_collon"}
-else{
-    Clear-Host
-    $Drive_Letter               = Read-Host 'Enter you project driver letter only'
-    $Global:Drive_Letter_collon = "$Drive_Letter" + ':\'
-    $Test_Drive                 = Test-path $Global:Drive_Letter_collon
-    Clear-Host}
-    if ($Test_Drive.Equals($true)){
-    Clear-Host
-    Write-Host 'You Drive is set to' "$Global:Drive_Letter_collon"}
-        else{
-        Clear-Host
-        $Drive_Letter               = Read-Host 'Enter you project driver letter only'
-        $Global:Drive_Letter_collon = "$Drive_Letter" + ':\'
-        $Test_Drive                 = Test-path $Global:Drive_Letter_collon
-        Clear-Host}
-            if ($Test_Drive.Equals($true)){
-        Clear-Host
-        Write-Host 'You Drive is set to' "$Global:Drive_Letter_collon"}
-            else {Write-Host "you tried 3 wrong times, Please try again later"}
+if ( $AskForVMNumber -ge 1) 
+{ 
 
-##### Create Project folder #####        
+    $Global:VM_Name = for ( $Number = $AskForVMNumber.Length ; $Number -le $AskForVMNumber ; $Number ++ )
+    {
+    Read-Host "Please enter your vm number $Number name"
+    }
+ }
+
+ Add-Content -Path $ExportFolderPath\VM.txt -Value $VM_Name -Force
+#endregion
+
+#region Set ISO path
+Clear-Host ; $Global:ISOPATH = Read-Host 'Please enter your Windows ISO location path'
+$Global:ISOLocalFolderPath     = 'c:\ISO'
+$Global:TestISOLocalFolderPath = Test-Path $ISOFolderPath
+if ($TestISOLocalFolderPath -eq $false) 
+{New-Item -Name 'ISO' -ItemType Directory -Path $Drive_Letter_collon -Force}
+$Global:ISOFiles = Get-ChildItem -Path $ISOLocalFolderPath -Filter *.iso
+$Global:ISOName  = $ISOFiles.Name
+#endregion
+
+#region Create Project folder
 $Test_Project_Path = Test-Path -Path $Global:Project_Path
 if ($Test_Project_Path.Equals($false))
 {
@@ -47,24 +74,42 @@ Write-Host "We made a Project Folder on behalf of you to create your VM's inside
 }
 elseif ($Test_Project_Path.Equals($true))
 {Clear-Host}
+#endregion
 
-###### Create Project Private switch ######        
+#region Create Project switch
 Clear-Host
 $VM_Switches = Get-VMSwitch | Out-String
 if ($VM_Switches.Contains($Global:Project_Name))
 {Write-Host "We found that your VM switch is already created before"}
 else 
 {New-VMSwitch "$Global:Project_Name" -SwitchType Private}
+$Global:ProjectSwitch = Get-VMSwitch -Name $Project_Name | select Name,SwitchType
+#endregion
 
-####### Print Project summary #######   
+#region Print Project summary
 Clear-Host
 Write-Host "         "
 Write-Host "         "
-Write-Host "Your drive is '$Global:Drive_Letter_collon'"
-Write-Host "Your project name is '$Global:Project_Name'"
-Write-Host "Your project private switch name is '$Global:Project_Name'"
-Write-Host "Your VM name is  '$Global:VM_Full_Name'"
-Write-Host "Your VM path is '$Global:VM_Path'"
+Write-Host "Your drive is 
+
+'$Drive_Letter_collon'"
 Write-Host "         "
 Write-Host "         "
-        
+Write-Host "Your project name is 
+
+'$Project_Name'"
+Write-Host "         "
+Write-Host "         "
+Write-Host "Your VM path is '$Project_Path'"
+$VM_Name2 = $VM_Name
+Write-Host "         "
+Write-Host "         "
+Write-Host Your VM name is  
+foreach ( $VM_Name2 in $VM_Name2 ) {Write-Host $Project_Name-$VM_Name2}
+Write-Host "         "
+Write-Host "         "
+Write-Host "Your project switch name is"
+Get-VMSwitch -Name $Project_Name | select Name,SwitchType | ft -AutoSize
+Remove-Item -Path $ExportFolderPath\$VMTXTFileNAme -Recurse -Force
+#endregion
+
